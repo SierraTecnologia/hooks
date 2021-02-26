e6717322919c3ac7754c82c79ce845b64d0b6f6b
ricardo@sierratecnologia.com.br <ricardo@sierratecnologia.com.br>
2021-02-26 17:35:26 -0300

    CAMN-10

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Setup the test environment.
      */
-    protected function setUp(): void: void
+    protected function setUp(): void
     {
         parent::setUp();
 
